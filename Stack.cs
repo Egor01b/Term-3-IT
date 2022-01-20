a0e07a4a1fbba3b256c106549d182be58f7da800
Egor Belous <belousegor5@gmail.com>
2022-01-20 20:08:23 +0200

    Create Template for Stack

---
+++ Stack.cs
@@ -0,0 +1,21 @@
+namespace Algorithms1;
+
+public class Stack
+{
+    private Element _top;
+
+    public void Push(string value)
+    {
+    }
+
+    public string Pop()
+    {
+        return "";
+    }
+
+    private class Element
+    {
+        public Element Next;
+        public string Value;
+    }
+}
